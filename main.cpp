ea15c57b38ea2fefb41879b841dc7bb8b14d1670
laurencefass <laurencefass@laurencefass-HP-G70-Notebook-PC>
2013-02-08 13:32:40 +0000

    first commit main.cpp

---
+++ main.cpp
@@ -0,0 +1,24 @@
+/* 
+ * File:   main.cpp
+ * Author: laurencefass
+ *
+ * Created on 08 February 2013, 13:19
+ */
+
+#include <cstdlib>
+#include <iostream>
+
+using namespace std;
+
+void hello()
+{
+    cout << "hello world";
+}
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    hello();
+    return 0;
+}
